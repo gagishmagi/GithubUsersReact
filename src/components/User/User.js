229bb4fd6db1f69b0516a276dd051ed9e0ed1adb
Gagi Shmagi <gagi.shmagi@gmail.com>
2021-08-11 14:03:58 +0300

    Added Custom bulma component for Card

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,16 +1,18 @@
+import Card from "../Bulma/Card"
 import "./User.css"
 
 const User = ({ user }) => {
-
+  console.log(user)
   return (
     <div>
-      <ul>
+      <Card src={user.avatar_url}/>
+      {/* <ul>
         <img src={user.avatar_url} alt={user.avatar_url} />
         <li>Username: {user.login}</li>
         <li>Followers: {user.followers}</li>
         <li>Following: {user.following}</li>
         <li>Created at: {user.created_at}</li>
-      </ul>
+      </ul> */}
     </div>
   );
 };
